c6a57af1415093fdf34877c41df21a5547a029c6
kjaindevops <122005370+kjaindevops@users.noreply.github.com>
2023-07-03 20:05:16 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,8 +1,6 @@
-def gitUrl = "https://github.com/kjaindevops/jenkins-jobs-group.git"
-
 pipelineJob('rnd-grpfnctn-seedjob') {
   description('Pipeline creation test')
   parameters{
-    choiceParam('gitUrl', [gitUrl], 'Jenkins git URL')
+    choiceParam('gitBranchType', ['feature', 'main', 'develop'])
   }
 }
